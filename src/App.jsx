44817226220a070ba9423fb284d11c5b06474c3f
Apple <apple@admins-MacBook-Pro.local>
2024-07-21 13:43:24 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <img src="https://cdn-media-1.freecodecamp.org/images/EDK8oFsr4DbyrL-I7C7U7MoRktH7oOIPHQuv" />
+    </>
+  );
+}
+
+export default App;
